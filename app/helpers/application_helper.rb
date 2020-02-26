26b9ac7241dc62ce03bce620a44093214d201072
Adrian Corcoran <adrian.corcoran@shopify.com>
2020-02-26 10:00:26 +0000

    Feedback from Chris, improve abstraction and get those damn EOLs :)

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,17 @@
 module ApplicationHelper
+  
+  def button_to_show(local_assigns)
+    # define the properties
+    use_method = local_assigns[:method] || :get
+    use_style_class = local_assigns[:style_class] ? ' ' + local_assigns[:style_class] : ''
+    enabled = local_assigns[:active_button] === false ? false : true
+    # set the default type to return
+    type = use_method
+    unless enabled
+      type = :disabled
+    end
+    # return hash
+    { :type => type, :use_style_class => use_style_class, :text => local_assigns[:text] }  
+  end
+
 end
