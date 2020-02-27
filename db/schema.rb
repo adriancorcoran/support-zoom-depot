60e870f8917bb7c3ead1047bf978709bf3ada5c5
Adrian Corcoran <adrian.corcoran@shopify.com>
2020-02-27 16:31:02 +0000

    Cart quantity, data migration and test update complete

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_27_145305) do
+ActiveRecord::Schema.define(version: 2020_02_27_152930) do
 
   create_table "carts", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
