513007258abcf04fd00916a8af15aad49276d3af
Adrian Corcoran <adrian.corcoran@shopify.com>
2020-03-05 14:18:52 +0000

    Add Active Job for charging orders, fix cases sensitivity bug in order_params

---
+++ app/jobs/charge_order_job.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+class ChargeOrderJob < ApplicationJob
+  queue_as :default
+
+  def perform(order, pay_type_params)
+    # Do something later
+    order.charge!(pay_type_params)
+  end
+end
